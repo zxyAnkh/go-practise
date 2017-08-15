45b5932ad7dea72b6d5365cf0ee8594455a3beae
127.0.0.1 <zxyankh@aliyun.com>
2017-08-15 19:50:19 +0800

    add protobuf practise

--- src/main.go
+++ src/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"./protobuf"
 	"fmt"
+	"github.com/golang/protobuf/proto"
 )
 
 func main() {
-	fmt.Println("Hello world.")
+	hw := &protobuf.Helloworld{
+		Id:  1,
+		Str: "2",
+		Opt: 3,
+	}
+	data, err := proto.Marshal(hw)
+	if err != nil {
+		fmt.Println("marshaling error")
+	}
+	fmt.Println(hw)
+	fmt.Println(data)
 }
